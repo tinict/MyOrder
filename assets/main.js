8295c8d25705b3093420459cec7064ce293c8da1
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-07-18 15:31:40 +0700

    test

--- assets/main.js
+++ assets/main.js
@@ -1,125 +1,276 @@
 var elementMinus = document.querySelectorAll('.fa-square-minus');
 var elementPlus = document.querySelectorAll('.fa-square-plus');
 var numIndex = document.querySelectorAll('.count');
-
+var costElement = document.querySelectorAll('.cost');
+var sum = 0;
+function sumDrink(tong){
+  var sumDrink = document.querySelector('.money-pay');
+  sumDrink.textContent = String(tong);
+}
 elementMinus[0].onclick = function(){
   console.log(numIndex[0].textContent = String(Number(numIndex[0].textContent) - 1));
+  var costDrink = Number(costElement[0].textContent);
+  sum -= costDrink;
+  sumDrink(sum);
+  console.log(sum);
 }
 elementPlus[0].onclick = function(){
   console.log(numIndex[0].textContent = String(Number(numIndex[0].textContent) + 1));
+  var costDrink = Number(costElement[0].textContent);
+  sum += costDrink;
+  sumDrink(sum);
+  console.log(sum);
 }
 
 elementMinus[1].onclick = function(){
   console.log(numIndex[1].textContent = String(Number(numIndex[1].textContent) - 1));
+  var costDrink = Number(costElement[1].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[1].onclick = function(){
   console.log(numIndex[1].textContent = String(Number(numIndex[1].textContent) + 1));
+  var costDrink = Number(costElement[1].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[2].onclick = function(){
   console.log(numIndex[2].textContent = String(Number(numIndex[2].textContent) - 1));
+  var costDrink = Number(costElement[2].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[2].onclick = function(){
   console.log(numIndex[2].textContent = String(Number(numIndex[2].textContent) + 1));
+  var costDrink = Number(costElement[2].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[3].onclick = function(){
   console.log(numIndex[3].textContent = String(Number(numIndex[3].textContent) - 1));
+  var costDrink = Number(costElement[3].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[3].onclick = function(){
   console.log(numIndex[3].textContent = String(Number(numIndex[3].textContent) + 1));
+  var costDrink = Number(costElement[3].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[4].onclick = function(){
   console.log(numIndex[4].textContent = String(Number(numIndex[4].textContent) - 1));
+  var costDrink = Number(costElement[4].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[4].onclick = function(){
   console.log(numIndex[4].textContent = String(Number(numIndex[4].textContent) + 1));
+  var costDrink = Number(costElement[4].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[5].onclick = function(){
   console.log(numIndex[5].textContent = String(Number(numIndex[5].textContent) - 1));
+  var costDrink = Number(costElement[5].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[5].onclick = function(){
   console.log(numIndex[5].textContent = String(Number(numIndex[5].textContent) + 1));
+  var costDrink = Number(costElement[5].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[6].onclick = function(){
   console.log(numIndex[6].textContent = String(Number(numIndex[6].textContent) - 1));
+  var costDrink = Number(costElement[6].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[6].onclick = function(){
   console.log(numIndex[6].textContent = String(Number(numIndex[6].textContent) + 1));
+  var costDrink = Number(costElement[6].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[7].onclick = function(){
   console.log(numIndex[7].textContent = String(Number(numIndex[7].textContent) - 1));
+  var costDrink = Number(costElement[7].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[7].onclick = function(){
   console.log(numIndex[7].textContent = String(Number(numIndex[7].textContent) + 1));
+  var costDrink = Number(costElement[7].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[8].onclick = function(){
   console.log(numIndex[8].textContent = String(Number(numIndex[8].textContent) - 1));
+  var costDrink = Number(costElement[8].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[8].onclick = function(){
   console.log(numIndex[8].textContent = String(Number(numIndex[8].textContent) + 1));
+  var costDrink = Number(costElement[8].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[9].onclick = function(){
   console.log(numIndex[9].textContent = String(Number(numIndex[9].textContent) - 1));
+  var costDrink = Number(costElement[9].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[9].onclick = function(){
   console.log(numIndex[9].textContent = String(Number(numIndex[9].textContent) + 1));
+  var costDrink = Number(costElement[9].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[10].onclick = function(){
   console.log(numIndex[10].textContent = String(Number(numIndex[10].textContent) - 1));
+  var costDrink = Number(costElement[10].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[10].onclick = function(){
   console.log(numIndex[10].textContent = String(Number(numIndex[10].textContent) + 1));
+  var costDrink = Number(costElement[10].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[11].onclick = function(){
   console.log(numIndex[11].textContent = String(Number(numIndex[11].textContent) - 1));
+  var costDrink = Number(costElement[11].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[11].onclick = function(){
   console.log(numIndex[11].textContent = String(Number(numIndex[11].textContent) + 1));
+  var costDrink = Number(costElement[11].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[12].onclick = function(){
   console.log(numIndex[12].textContent = String(Number(numIndex[12].textContent) - 1));
+  var costDrink = Number(costElement[12].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[12].onclick = function(){
   console.log(numIndex[12].textContent = String(Number(numIndex[12].textContent) + 1));
+  var costDrink = Number(costElement[12].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[13].onclick = function(){
   console.log(numIndex[13].textContent = String(Number(numIndex[13].textContent) - 1));
+  var costDrink = Number(costElement[13].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[13].onclick = function(){
   console.log(numIndex[13].textContent = String(Number(numIndex[13].textContent) + 1));
+  var costDrink = Number(costElement[13].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[14].onclick = function(){
   console.log(numIndex[14].textContent = String(Number(numIndex[14].textContent) - 1));
+  var costDrink = Number(costElement[14].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[14].onclick = function(){
   console.log(numIndex[14].textContent = String(Number(numIndex[14].textContent) + 1));
+  var costDrink = Number(costElement[14].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[15].onclick = function(){
   console.log(numIndex[15].textContent = String(Number(numIndex[15].textContent) - 1));
+  var costDrink = Number(costElement[15].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[15].onclick = function(){
   console.log(numIndex[15].textContent = String(Number(numIndex[15].textContent) + 1));
+  var costDrink = Number(costElement[15].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 
 elementMinus[16].onclick = function(){
   console.log(numIndex[16].textContent = String(Number(numIndex[16].textContent) - 1));
+  var costDrink = Number(costElement[16].textContent);
+  sum -= costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
 elementPlus[16].onclick = function(){
   console.log(numIndex[16].textContent = String(Number(numIndex[16].textContent) + 1));
+  var costDrink = Number(costElement[16].textContent);
+  sum += costDrink;
+  console.log(sum);
+  sumDrink(sum);
 }
+
+var moneyUser = document.querySelector('input[type="text"]');
+var cashback = document.querySelector('.money-back');
+
+
+
+moneyUser.onchange = function(e){
+  cashback.textContent = String(Number(e.target.value) - sum);
+}
+
 
 
 

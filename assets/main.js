d0da5e62191837237dc935a2d38bb38c7a03cdb0
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-07-18 12:15:44 +0700

    test7

--- assets/main.js
+++ assets/main.js
@@ -16,4 +16,110 @@ elementPlus[1].onclick = function(){
   console.log(numIndex[1].textContent = String(Number(numIndex[1].textContent) + 1));
 }
 
+elementMinus[2].onclick = function(){
+  console.log(numIndex[2].textContent = String(Number(numIndex[2].textContent) - 1));
+}
+elementPlus[2].onclick = function(){
+  console.log(numIndex[2].textContent = String(Number(numIndex[2].textContent) + 1));
+}
+
+elementMinus[3].onclick = function(){
+  console.log(numIndex[3].textContent = String(Number(numIndex[3].textContent) - 1));
+}
+elementPlus[3].onclick = function(){
+  console.log(numIndex[3].textContent = String(Number(numIndex[3].textContent) + 1));
+}
+
+elementMinus[4].onclick = function(){
+  console.log(numIndex[4].textContent = String(Number(numIndex[4].textContent) - 1));
+}
+elementPlus[4].onclick = function(){
+  console.log(numIndex[4].textContent = String(Number(numIndex[4].textContent) + 1));
+}
+
+elementMinus[5].onclick = function(){
+  console.log(numIndex[5].textContent = String(Number(numIndex[5].textContent) - 1));
+}
+elementPlus[5].onclick = function(){
+  console.log(numIndex[5].textContent = String(Number(numIndex[5].textContent) + 1));
+}
+
+elementMinus[6].onclick = function(){
+  console.log(numIndex[6].textContent = String(Number(numIndex[6].textContent) - 1));
+}
+elementPlus[6].onclick = function(){
+  console.log(numIndex[6].textContent = String(Number(numIndex[6].textContent) + 1));
+}
+
+elementMinus[7].onclick = function(){
+  console.log(numIndex[7].textContent = String(Number(numIndex[7].textContent) - 1));
+}
+elementPlus[7].onclick = function(){
+  console.log(numIndex[7].textContent = String(Number(numIndex[7].textContent) + 1));
+}
+
+elementMinus[8].onclick = function(){
+  console.log(numIndex[8].textContent = String(Number(numIndex[8].textContent) - 1));
+}
+elementPlus[8].onclick = function(){
+  console.log(numIndex[8].textContent = String(Number(numIndex[8].textContent) + 1));
+}
+
+elementMinus[9].onclick = function(){
+  console.log(numIndex[9].textContent = String(Number(numIndex[9].textContent) - 1));
+}
+elementPlus[9].onclick = function(){
+  console.log(numIndex[9].textContent = String(Number(numIndex[9].textContent) + 1));
+}
+
+elementMinus[10].onclick = function(){
+  console.log(numIndex[10].textContent = String(Number(numIndex[10].textContent) - 1));
+}
+elementPlus[10].onclick = function(){
+  console.log(numIndex[10].textContent = String(Number(numIndex[10].textContent) + 1));
+}
+
+elementMinus[11].onclick = function(){
+  console.log(numIndex[11].textContent = String(Number(numIndex[11].textContent) - 1));
+}
+elementPlus[11].onclick = function(){
+  console.log(numIndex[11].textContent = String(Number(numIndex[11].textContent) + 1));
+}
+
+elementMinus[12].onclick = function(){
+  console.log(numIndex[12].textContent = String(Number(numIndex[12].textContent) - 1));
+}
+elementPlus[12].onclick = function(){
+  console.log(numIndex[12].textContent = String(Number(numIndex[12].textContent) + 1));
+}
+
+elementMinus[13].onclick = function(){
+  console.log(numIndex[13].textContent = String(Number(numIndex[13].textContent) - 1));
+}
+elementPlus[13].onclick = function(){
+  console.log(numIndex[13].textContent = String(Number(numIndex[13].textContent) + 1));
+}
+
+elementMinus[14].onclick = function(){
+  console.log(numIndex[14].textContent = String(Number(numIndex[14].textContent) - 1));
+}
+elementPlus[14].onclick = function(){
+  console.log(numIndex[14].textContent = String(Number(numIndex[14].textContent) + 1));
+}
+
+elementMinus[15].onclick = function(){
+  console.log(numIndex[15].textContent = String(Number(numIndex[15].textContent) - 1));
+}
+elementPlus[15].onclick = function(){
+  console.log(numIndex[15].textContent = String(Number(numIndex[15].textContent) + 1));
+}
+
+elementMinus[16].onclick = function(){
+  console.log(numIndex[16].textContent = String(Number(numIndex[16].textContent) - 1));
+}
+elementPlus[16].onclick = function(){
+  console.log(numIndex[16].textContent = String(Number(numIndex[16].textContent) + 1));
+}
+
+
 
